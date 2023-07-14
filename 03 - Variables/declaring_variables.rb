0d34b010e6d01ae39122317f9e87be74825002b4
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-13 23:09:33 -0400

    declaring variables

---
+++ 03 - Variables/declaring_variables.rb
@@ -0,0 +1,23 @@
+age = 45
+puts age
+
+name = "Matthew"
+last_name = "Woodard"
+puts name + " " + last_name
+
+puts age + 5
+
+
+age = 30
+puts age
+
+age_in_ten_years = age + 10
+puts age_in_ten_years
+
+age = age + 7 
+puts age
+
+chameleon = 24
+chameleon = "Matthew"
+chameleon = 3.14
+puts chameleon
