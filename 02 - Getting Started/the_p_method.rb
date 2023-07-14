9da92e3de619475352c0ec8ed5c79fec9689158c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-13 22:21:18 -0400

    print and p methods

---
+++ 02 - Getting Started/the_p_method.rb
@@ -0,0 +1,14 @@
+puts "Matthew Woodard" # prints Matthew Woodard on a new line
+p "Matthew Woodard" # prints "Matthew Woodard" on a new line
+
+puts 5            # prints 5 on a new line
+puts "5"           # prints 5 on a new line
+
+p 5              # prints 5 on a new line
+p "5"            # prints "5" on a new line
+
+p 5 + 5            # prints 10 on a new line
+p "5 + 5"          # prints "5 + 5" on a new line
+
+puts "Hi there\nline break"     # "Hi there" on one line, "line break" on the next
+p "Hi there\nline break"    # "Hi there\nline break"
---
+++ 02 - Getting Started/the_print_method.rb
@@ -0,0 +1,4 @@
+print "This random nonsense"
+print "will be printed on the same line."
+
+#output: This random nonsensewill be printed on the same line.
