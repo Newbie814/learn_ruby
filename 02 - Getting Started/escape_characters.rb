cd1cd7d3aa5331850fdcbf1fc44a3cb837f35f5a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-13 22:09:08 -0400

    puts and escape characters

---
+++ 02 - Getting Started/escape_characters.rb
@@ -0,0 +1,10 @@
+#line break  \n
+puts "some text \nmore text"
+
+#tab \t
+puts "\tOnce upon a time"
+
+#double quotes \"
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said "Goodbye" to Romeo'
+puts "Juliet said 'Goodbye' to Romeo" #Goodbye would be wrapped in single quotes in output
