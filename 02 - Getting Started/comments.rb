f1087b5e99bb29f6e1493d598c1028d5b84d7c2a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-13 22:28:44 -0400

    comments and mutliline comments

---
+++ 02 - Getting Started/comments.rb
@@ -0,0 +1,3 @@
+# ruby ignores comments. used to explain or add notes to code
+# can be temporarily disable code
+puts "hello world" # running this file will only run this line of code
---
+++ 02 - Getting Started/multiline_comments.rb
@@ -0,0 +1,10 @@
+=begin
+  Hello
+  This is a multiline comment
+  It is very useful
+  code after end
+  
+=end
+
+puts "Hello World"
+puts 1 + 1
