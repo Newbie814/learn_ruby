c4e66bf70e544f9f6bd13d82f8f44107d25f028e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-16 16:12:55 -0400

    intro to object methods, integer methods

---
+++ 04 - Object Methods/integer_methods.rb
@@ -0,0 +1,7 @@
+puts 10.next # 11
+puts -1.next # 0
+puts 10.succ # 11  --- shorthand for successor
+
+puts 10.to_s # "10"
+puts 10.pred # 9 --- shorthand for predecessor
+puts 10.pred # 9
---
+++ 04 - Object Methods/intro_to_object_methods.rb
@@ -0,0 +1,16 @@
+puts "hello world".length # 11
+puts "hello world".upcase # HELLO WORLD
+puts "TOTALLY AWESOME".downcase # totally awesome
+
+
+expression = "TOTALLY AWESOME"
+puts expression.downcase # totally awesome
+puts # empty line
+# below also works, but it is convention to leave of the parentheses
+puts "hello world".length() # 11
+puts "hello world".upcase() # HELLO WORLD
+puts "TOTALLY AWESOME".downcase() # totally awesome
+
+
+expression = "TOTALLY AWESOME"
+puts expression.downcase() # totally awesome
