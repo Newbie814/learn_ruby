b6ca6da3c2c9679735f3c6b9a17f2d4e0dd10182
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-16 16:31:04 -0400

    inspect method and method chaining

---
+++ 04 - Object Methods/method_chaining.rb
@@ -0,0 +1,4 @@
+puts "hi there".upcase.reverse # EREHT IH
+puts "hi there".upcase.length.succ # 9
+
+puts 10.next.next.pred # 11
---
+++ 04 - Object Methods/the_inspect_method.rb
@@ -0,0 +1,5 @@
+# a normal use case for the inspect method is to use it to debug your code
+puts "Hello World\n" 
+puts "Hello World\n".inspect # will print out the string with the escape character
+
+p "Hello World\n" # p is equivalent to puts.inspect, so this is the same as line 3 above
