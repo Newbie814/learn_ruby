92962cd99da802176b4e2a28aea0bd865a2c6bc3
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-16 16:20:14 -0400

    constants, nomethoderror

---
+++ 04 - Object Methods/exceptions_nomethoderror.rb
@@ -0,0 +1,4 @@
+# puts "hello".lenth
+# above line throws an error because there is no lenth method(mispelled method)
+# puts 5.length 
+# throws an error because 5 is an integer and integers do not have a length method
