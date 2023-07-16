d83892ff86e9a6cad418be10e725ee97854ed75a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-16 17:02:36 -0400

    constants,

---
+++ 03 - Variables/constants.rb
@@ -0,0 +1,9 @@
+pi = 3.14159 # this is a variable, which means value can change
+
+Pi = 3.14159 # this is a constant, which means value cannot change
+
+PI = 3.14159 # all that is required is that the first letter be capitalized, but it is common practice to capitalize all letters
+
+TAX_RATE = 0.05 # constants are used for values that are not expected to change
+
+puts "pi = #{pi}", "Pi = #{Pi}", "PI = #{PI}", "TAX_RATE = #{TAX_RATE}"
